8f0f5832903c63e34106e23443eabae484993c9d
Manith Marapperuma <39-bce-0006@kdu.ac.lk>
2024-03-04 23:56:17 +0530

    modified

--- app.py
+++ app.py
@@ -3,10 +3,8 @@ import tensorflow as tf
 import numpy as np
 import PIL
 
-# Load the saved model
 model = tf.keras.models.load_model('oily_dry.h5')
 
-# Define the function for image classification
 def classify_image(image_path):
     # Load the image to be classified
     image = PIL.Image.open(image_path).resize((224, 224))
@@ -19,13 +17,22 @@ def classify_image(image_path):
     # Classify the image
     predictions = model.predict(image)
 
-    # Get the predicted class label
-    predicted_class = np.argmax(predictions)
+    # Get the predicted class label and percentages
+    predicted_class, dry_percentage, oily_percentage, normal_percentage = np.argmax(predictions), float(predictions[0][0]), float(predictions[0][1]), float(predictions[0][2])
+
+    # Ensure percentages are within the valid range (0 to 100)
+    dry_percentage = max(0, min(dry_percentage, 1))
+    oily_percentage = max(0, min(oily_percentage, 1))
+    normal_percentage = max(0, min(normal_percentage, 1))
+
+    # Convert percentages to strings with two decimal places
+    dry_percentage_str = f"{dry_percentage*100:.2f}"
+    oily_percentage_str = f"{oily_percentage*100:.2f}"
+    normal_percentage_str = f"{normal_percentage*100:.2f}"
 
     # Return the predicted class label and percentages
-    return predicted_class, float(predictions[0][0]), float(predictions[0][1])
+    return predicted_class, dry_percentage_str, oily_percentage_str, normal_percentage_str
 
-# Define the Streamlit app
 def app():
     st.title("Oily/Dry Skin Level Predictor 🧑🏻👩🏻")
     st.write("Coded by Manith Jayaba")
@@ -41,25 +48,10 @@ def app():
         with open(image_path, "wb") as f:
             f.write(image_file.getbuffer())
 
+        predicted_class, dry_percentage, oily_percentage, normal_percentage = classify_image(image_path)
+
         st.image(image_path, width=250)
 
-        predicted_class, dry_percentage, oily_percentage = classify_image(image_path)
-
-        dry_percentage = dry_percentage # Replace with your actual dry percentage value
-        oily_percentage = oily_percentage  # Replace with your actual oily percentage value
-
-        # Ensure percentages are within the valid range (0 to 100)
-        dry_percentage = max(0, min(dry_percentage, 1))
-        oily_percentage = max(0, min(oily_percentage, 1))
-
-        # Convert percentages to strings with two decimal places
-        dry_percentage_str = f"{dry_percentage*100:.2f}"
-        oily_percentage_str = f"{oily_percentage*100:.2f}"
-
-        st.progress(dry_percentage,text="You have "+dry_percentage_str+"% Dry Skin")
-
-        st.progress(oily_percentage,text="You have "+oily_percentage_str+"% Oily Skin")
-
-# Run the Streamlit app
-if __name__ == "__main__":
-    app()
+        st.progress(dry_percentage,text="You have "+dry_percentage+"% Dry Skin")
+        st.progress(oily_percentage,text="You have "+oily_percentage+"% Oily Skin")
+        st.progress(normal_percentage,text="You have "+normal_percentage+"% Normal Skin")
